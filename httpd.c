83d576db50b70f2f531b7e16a9117faa90632280
gaoxiaodiao <805888196@qq.com>
2017-08-01 16:40:53 +0800

    http服务器:cgi框架搭建完成!

--- httpd.c
+++ httpd.c
@@ -9,12 +9,98 @@ static const char* respond_line = "HTTP/1.0 200 OK\r\n";
 static const char* blank_line = "\r\n";
 
 
-//处理CGI
+//CGI处理
 void exec_cgi(int sock,char *method,char *path){
-	
+	char buff[1024] = {0};
+	char * query_string = NULL;
+	int len = 0;
+	int ret = 0;
+	//获取参数
+	if(strcasecmp(method,"GET") == 0){
+		//当前为get请求,从path提取请求参数
+		char *tmp = strchr(path,'?');
+		if(tmp!=NULL){
+			query_string = tmp+1;
+		}
+		if(*query_string=='\0' || strchr(path,'=')==NULL ){
+			//虽然存在?但不存在=,当作简单的get请求去处理
+			*tmp = '\0';
+			handle_simple_get(sock,path);
+			return ;
+		}
+		do{
+			ret = get_line(sock,buff,sizeof(buff));
+			printf("[GET]%s",buff);
+		}while(ret!=1);
+	}else{
+		//当前为post请求,从请求正文中提取请求参数
+		do{
+			ret = get_line(sock,buff,sizeof(buff));
+			printf("[POST]%s",buff);
+			if(strncasecmp(buff,"Content-Length: ",16) == 0){
+				char *space = strchr(buff,' ');
+				if(space == NULL){
+					printf("space is NULL\n");
+					return ;
+				}
+				len = atoi(space);
+			}
+		}while(ret!=1);
+		printf("i get the len:%d\n",len);
+		//读post的请求参数
+		ret = recv(sock,buff,len,0);
+		buff[len] = '\0';
+		if(ret == 0){
+			printf("[POST]the data is over\n");
+			return ;
+		}else if(ret < 0){
+			perror("[POST]get data fail");
+			return ;
+		}
+		query_string = buff;
+	}
+	printf("query_string:%s\n",query_string);
+	int input[2];
+	int output[2];
+	if(pipe(input) < 0){
+		perror("pipe");
+		return ;
+	}
+	if(pipe(output) < 0){
+		perror("pipe");
+		return ;
+	}
+	pid_t pid = fork();
+	if(pid == 0){		//child
+		close(input[1]);
+		close(output[0]);
+		dup2(input[0],0);
+		dup2(output[1],1);
+		execl(path,path,NULL);
+	}else if(pid > 0){	//father
+		close(input[0]);
+		close(output[1]);
+		char buff[4096] = {0};
+		//将请求信息写入管道
+		write(input[1],query_string,strlen(query_string));
+		printf("father request info is written in pipe\n");
+		//从管道中读出信息发送给client	
+		send(sock,respond_line,strlen(respond_line),0);
+		send(sock,blank_line,strlen(blank_line),0);
+		ssize_t s = 0;
+		do{
+			s = read(output[0],buff,sizeof(buff));
+			buff[s] = 0;
+			send(sock,buff,s,0);
+			printf("buff is %s",buff);
+		}while(s != 0);
+		wait(NULL);
+		close(input[1]);
+		close(output[0]);
+	}
 }
 //处理简单的get请求
-void handle_simple_get(int sock,const char *path){
+void handle_simple_get(int sock,char *path){
 	//读取多余请求
 	int ret = 1;
 	char buff[1024];
@@ -28,6 +114,15 @@ void handle_simple_get(int sock,const char *path){
 	if(stat(path,&file_info) < 0){
 		//文件不存在,返回404状态码
 		send_error(sock,404);
+		return ;
+	}
+	if(S_ISDIR(file_info.st_mode)){
+		//当前请求的为目录
+		sprintf(path,"%s/index.html",path);
+	}
+	if(file_info.st_mode & S_IXOTH){
+		//当前请求存在可执行权限
+		printf("path=%s\n",path);
 		return ;
 	}
 	int fd = open(path,O_RDONLY);
@@ -138,7 +233,9 @@ void handle_http_request(int sock){
 	if(strcasecmp(method,"GET")==0 && strchr(path,'?')==NULL){
 		//GET请求 且 不带参数
 		handle_simple_get(sock,full_path);
+		return ;
 	}
+	printf("need cgi!\n");
 	//GET请求带参数 或者 POST请求,需要用到CGI
 	exec_cgi(sock,method,full_path);
 end:
