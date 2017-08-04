6a583070cfc42fd009c4bd00ad24b019abaf4a60
gaoxiaodiao <805888196@qq.com>
2017-08-04 14:12:39 +0800

    修复小Bug

--- wwwroot/cgi-bin/lib/Spider.py
+++ wwwroot/cgi-bin/lib/Spider.py
@@ -9,15 +9,19 @@
 import Downloader,UrlManager
 import threading
 from urlparse import urljoin
+from urlparse import urlparse
 from bs4 import BeautifulSoup
+import sys
+sys.path.append("..")
+from modules import sql_check
 class Spider:
     def __init__(self,root,thread_num):
         self.urls = UrlManager.UrlManager()
         self.downloader = Downloader.Downloader()
         self.root = root
         self.thread_num = thread_num
-    
-    #判断url是否属于domain
+        self.web_file = set();
+    #判断url是否在同一域名下
     def _judge(self,domain,url):
         if (url.find(domain) != -1):
             return True
@@ -34,13 +38,21 @@ class Spider:
         for link in links:
             new_url = link.get("href")
             new_full_url = urljoin(page_url,new_url)    
-            #此处可对url进行过滤,以获取特定的url
+            #################url过滤####################
+            if(new_full_url.find("?") != -1):
+                url_obj = urlparse(new_full_url)   
+                no_prama_url = "http://" + url_obj.netloc + url_obj.path
+                if(self.web_file is not None and no_prama_url in self.web_file):
+                    continue
+                self.web_file.add(no_prama_url)
+            ###########################################
             if(self._judge(self.root,new_full_url)):
                 new_urls.add(new_full_url)
         return new_urls
 
     #爬取
     def craw(self):
+        dynamic_url = set()
         self.urls.add_new_url(self.root)
         #只要url管理器中还存在新url,则不断的爬取
         while self.urls.has_new_url():
@@ -50,13 +62,16 @@ class Spider:
                 if self.urls.has_new_url() is False:
                     #url管理器中没有新url,则跳出循环
                     break;
-            new_url = self.urls.get_new_url()
-            
-            print "[Spider]",new_url
-
-            t = threading.Thread(target=self.downloader.down,args=(new_url,contents))
-            t.start()
-            threads.append(t)
+                new_url = self.urls.get_new_url()
+                ###############爬虫执行动作#####################
+                if(new_url.find("?") != -1):
+                    print "[Spider]",new_url
+                    dynamic_url.add(new_url)
+                    #sql_check.run(new_url)
+                ################################################
+                t = threading.Thread(target=self.downloader.down,args=(new_url,contents))
+                t.start()
+                threads.append(t)
             for thread in threads:
                 thread.join()
             for _str in contents:
@@ -64,3 +79,5 @@ class Spider:
                     continue
                 new_urls = self._get_new_urls(new_url,_str['html'])
                 self.urls.add_new_urls(new_urls)
+        print "爬取完毕,将结果返回"
+        return dynamic_url
