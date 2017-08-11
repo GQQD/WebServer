b25c25528dcf14287b8db537742a52f138c04db9
gaoxiaodiao <805888196@qq.com>
2017-08-11 10:26:24 +0800

    测试检测webcms

--- wwwroot/cgi-bin/Test.py
+++ wwwroot/cgi-bin/Test.py
@@ -13,39 +13,40 @@ from sys import argv
 from lib import Spider
 from modules import sql_check
 from modules import webcms_check
+from lib import Downloader
 #测试检查页面是否存在sql注入
 def test_sql_check():
     #ret = sql_check.run("http://www.hbxffy1.com/info/dispnews.asp?id=1709")
     #ret = sql_check.run("http://www.szxcc.com/gb/about1_xinxi.asp?id=325")
     ret = sql_check.run("http://www.szxcc.com/gb/zxgg_detail.asp?id=376")
-    print ret;
+    print(ret);
     #ret = sql_check.run("http://www.chinaxinge.com/company/skin/12/index.asp?id=7798")
     #print ret;
 
 #测试从url中获取域名
 def test_get_domain():
-    print cdn_check.get_domain("http://blog.csdn.net/imzoer/article/details/8636764")
-    print cdn_check.get_domain("http://www.cnblogs.com/allenblogs/archive/2011/11/15/2055149.html")
-    print cdn_check.get_domain("https://my.oschina.net/guol/blog/95699")
-    print cdn_check.get_domain("")
+    print (cdn_check.get_domain("http://blog.csdn.net/imzoer/article/details/8636764"))
+    print (cdn_check.get_domain("http://www.cnblogs.com/allenblogs/archive/2011/11/15/2055149.html"))
+    print (cdn_check.get_domain("https://my.oschina.net/guol/blog/95699"))
+    print (cdn_check.get_domain(""))
 
 #测试表单数据
 def test_get_post_request_info():
     url = "http://ce.cloud.360.cn"
     web_content = requests.get(url)
-    print cdn_check.get_post_request_info(web_content.text)
+    print (cdn_check.get_post_request_info(web_content.text))
 
 #测试cdn检测
 def test_cdn_check():
     script,first = argv
     msg,state=cdn_check.run(first)
-    print msg
+    print (msg)
 
 #测试端口扫描
 def test_port_scan():
     script,ip = argv
     if ip == None:
-        print "Usage:%s [ip]"%script
+        print ("Usage:%s [ip]"%script)
         return False
     ps = port_scan.PortScan(ip)
     ps.run()
@@ -60,7 +61,10 @@ def test_spider():
             break;
 #测试WebCMS指纹识别
 def test_webcms_check():
-    cms_check = webcms_check.webcms("http://www.szxcc.com/gb/") 
+    cms_check = webcms_check.webcms("http://www.szxcc.com/gb",10)
+    cms_check.run()
+    #down = Downloader.Downloader()
+    #print down.get("http://www.szxcc.com/gb/install/")
 if __name__ == '__main__':
     #test_get_domain()
     #test_get_post_request_info()
