72e7fced00d89987a5dfdd382a5660b6c6de9395
gaoxiaodiao <805888196@qq.com>
2017-08-12 19:32:56 +0800

    完成webshell爆破框架

--- wwwroot/cgi-bin/modules/webshell_check.py
+++ wwwroot/cgi-bin/modules/webshell_check.py
@@ -6,4 +6,29 @@
 *创建时间:2017年08月12日 星期六 18时31分26秒
 *开发环境:Kali Linux/Python v2.7.13
 """
+import os
+import sys
+sys.path.append("..")
+from lib import Downloader
 
+#加载数据字典到内存中
+filename = os.path.join(sys.path[0],'data','web_shell.dic')
+#print filename
+passwds = []
+fp = open(filename)
+for i in fp:
+    passwds.append(i.strip())
+def run(url):
+    if(not url.endswith(".php")):
+        return False
+    print '[Webshell_check]:%s'%url
+    post_data = {}
+    for passwd in passwds:
+       #some request 
+        # post_data[passwd] = 'echo "password is %s";'%passwd
+        downloader = Downloader.Downloader()
+        ret = downloader.post(url,post_data)
+        if(ret):
+            print "webshell:%s"%ret
+            return True
+    return False
