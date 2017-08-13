5b3d5716420a71a825cf4328ebb83d91dd3f3a1b
gaoxiaodiao <805888196@qq.com>
2017-08-13 09:53:08 +0800

    将xss语句加载至内存

--- wwwroot/cgi-bin/modules/xss_check.py
+++ wwwroot/cgi-bin/modules/xss_check.py
@@ -6,4 +6,13 @@
 *创建时间:2017年08月13日 星期日 09时45分42秒
 *开发环境:Kali Linux/Python v2.7.13
 """
+import sys
+import os
+#初始化操作,将xss注入语句加载至内存
+filename = os.path.join(sys.path[0],'data','xss.txt')
+f = open(filename)  #这里可能存在异常
+xss_code=[]
+for code in f:
+    print code
+    xss_code.append(code)
 
