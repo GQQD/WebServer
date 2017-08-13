f9b97398b241a60c9cfbbf40cade1c28c3e8143b
gaoxiaodiao <805888196@qq.com>
2017-08-13 10:24:15 +0800

    xss跨站脚本漏洞测试基本框架完成

--- wwwroot/cgi-bin/modules/xss_check.py
+++ wwwroot/cgi-bin/modules/xss_check.py
@@ -13,12 +13,16 @@ from lib import Downloader
 #初始化操作,将xss注入语句加载至内存
 filename = os.path.join(sys.path[0],'data','xss.txt')
 f = open(filename)  #这里可能存在异常
-xss_code=[]
+xss_codes=[]
 for code in f:
     #print code
-    xss_code.append(code)
+    xss_codes.append(code)
 
 #将url中的每个参数信息替换为xss_code,
+#例如:http://gaoxiaodiao.com/index.asp?id=1&cate=2
+#则会生成
+#http://gaoxiaodiao.com/index.asp?id=xss_code&cate=2
+#http://gaoxiaodiao.com/index.asp?id=1&cate=xss_code
 def urlsplit(url):
     domain = url.split("?")[0]  #取出?之前的东西
     _url = url.split("?")[1]    #获取参数部分
@@ -29,9 +33,23 @@ def urlsplit(url):
     
     urls = []
     for val in pararm.values():
-        new_url = domain + _url.replace(val,'xss_code')
+        new_url = domain +"?" +_url.replace(val,'xss_code')
         urls.append(new_url)
     return urls
 
 def run(url):
+    downloader = Downloader.Downloader()
+    urls = urlsplit(url)
+    if urls is None:
+        return False
+    for _url in urls:
+        for xss_code in xss_codes:
+            xss_test_url = _url.replace("xss_code",xss_code)
+            print "[xss_check]%s"%xss_test_url
+            html = downloader.get(xss_test_url)
+            if html is None:
+                continue
+            if(html.find(xss_code)!=-1):
+                print "[xss_check]xss found:%s"%xss_test_url
+    return False
     print "xss_check is running..." 
